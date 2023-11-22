e4c45ab81b1a7be47aa6b8f0dfcd60fa3737ccc4
Jason Stine <lostlaowai@outlook.com>
2023-11-21 22:56:51 -0800

    feat: serve the static files in the build directory

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { setupDatabase } from "./db/db-connection";
+import { fileURLToPath } from "url";
+import path from "path";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,8 +11,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// For serving the static files from the React build directory.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Set the path to the React application's build directory. This assumes that the production-ready frontend code
+// is located in the 'client/dist' directory, which is a common convention for React applications.
+const REACT_BUILD_DIR = path.join(__dirname, "..", "client", "dist");
+
+// Serve the static files from the React build directory. This middleware enables the Express server to
+// serve the optimized, production build of the React app, including HTML, CSS, JavaScript, and any other static assets.
+app.use(express.static(REACT_BUILD_DIR));
+
+// Serve the frontend
 app.get("/", (req, res) => {
-  res.json({ message: "Hola, from My template ExpressJS with React-Vite" });
+  res.sendFile(path.join(REACT_BUILD_DIR, "index.html"));
 });
 
 // app.post("/addContact", async (req, res) => {
